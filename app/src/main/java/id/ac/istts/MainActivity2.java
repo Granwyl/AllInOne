4eba4204d048a2f9b913c6a60c45806e04b85520
nicholas2000 <sughapur@gmail.com>
2021-10-06 12:38:49 +0700

    Halaman Login

---
+++ app/src/main/java/id/ac/istts/MainActivity2.java
@@ -0,0 +1,14 @@
+package id.ac.istts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
